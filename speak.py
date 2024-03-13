225ec68371b7169b68b6b9930d546638c22ed299
Jarvis <xyz@gmail.com>
2024-03-13 10:34:24 +0530

    First Commit

---
+++ speak.py
@@ -0,0 +1,17 @@
+import pyttsx3
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+
+engine.setProperty('rate',150)
+engine.setProperty('voices',voices[0].id)
+
+def Speak(*args, **kargs):
+    audio = ""
+    for i in args:
+        audio += str(i)
+    print(audio)
+    engine.say(audio)
+    engine.runAndWait()
+
+# Speak("Hi, my name is jarvis")
